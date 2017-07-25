24a8e14cb39309084f9c568cbaad5426053a4226
Chazona Baum <chznbaum@gmail.com>
2017-07-24 22:11:43 -0400

    Configured generators

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module HundredDays
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
