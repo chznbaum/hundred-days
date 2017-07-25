34a494c54a99044a16efb0a09739dd877bca5dff
Chazona Baum <chznbaum@gmail.com>
2017-07-25 14:02:29 -0400

    Implemented Page titles

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Chazona's #100DaysOfCode Log"
+  end
+
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
