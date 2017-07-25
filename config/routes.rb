91950de1297151144240474ac416dc4c837947cb
Chazona Baum <chznbaum@gmail.com>
2017-07-25 14:10:13 -0400

    Removed extra home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
   get 'pages/about'
 
   get 'pages/contact'
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -8,11 +8,6 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-  end
-
-  test "should get home" do
-    get pages_home_url
-    assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
 
