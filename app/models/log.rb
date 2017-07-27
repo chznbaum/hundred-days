4e4f8fd2f085880a71937bd6f3fbb459432e40d2
Chazona Baum <chznbaum@gmail.com>
2017-07-27 13:55:14 -0400

    Set up enums for Logs

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,15 @@
 class Log < ApplicationRecord
+  enum status: { "Draft" => 0, "Published" => 1 }
+  enum motivation: {
+    "😫" => 0,
+    "😖" => 1,
+    "😞" => 2,
+    "😕" => 3,
+    "🙄" => 4,
+    "😐" => 5,
+    "🤔" => 6,
+    "🙂" => 7,
+    "😉" => 8,
+    "😆" => 9
+  }
 end
