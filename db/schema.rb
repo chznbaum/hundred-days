a630cc632779680884973662c48f27a302c427e0
Chazona Baum <chznbaum@gmail.com>
2017-07-27 12:50:37 -0400

    Generated Logs via scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727152822) do
+ActiveRecord::Schema.define(version: 20170727163158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "logs", force: :cascade do |t|
+    t.integer "round"
+    t.integer "day"
+    t.text "content"
+    t.integer "status", default: 0
+    t.integer "motivation", default: 5
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
