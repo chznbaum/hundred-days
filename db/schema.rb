0849e631fed5f02e2acbf5f86eb9a889740d61a5
Chazona Baum <chznbaum@gmail.com>
2017-09-23 15:20:03 -0400

    added comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728030958) do
+ActiveRecord::Schema.define(version: 20170923191734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "commentable_id"
+    t.string "commentable_type"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "logs", force: :cascade do |t|
     t.integer "round"
@@ -45,4 +55,5 @@ ActiveRecord::Schema.define(version: 20170728030958) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
 end
