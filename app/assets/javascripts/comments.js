8b147789015a94298a93e7e50313c382bd3b492e
Chazona Baum <chznbaum@gmail.com>
2017-09-23 21:37:19 -0400

    hotfix: convert arrow function

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,17 +1,14 @@
 function commentReplyFunction() {
   const commentReplies = document.querySelectorAll('.reply');
-  console.log(commentReplies);
   function commentReplyExpand(event) {
     const expandSelector = ".expandme[data-commentid='" + this.dataset.commentid + "']";
-    console.log(expandSelector);
     const expandSection = document.querySelector(expandSelector);
     if (expandSection.classList.contains('hidden')) {
       expandSection.classList.remove('hidden');
     } else {
       expandSection.classList.add('hidden');
     }
-    console.log(expandSection);
   }
-  commentReplies.forEach(commentReply => commentReply.addEventListener('click', commentReplyExpand));
+  commentReplies.forEach(function(commentReply){ commentReply.addEventListener('click', commentReplyExpand) });
 }
 document.addEventListener('DOMContentLoaded', commentReplyFunction);
