432a46079158d7759b50b57ebe4299fb7bfa64d5
Chazona Baum <chznbaum@gmail.com>
2017-09-23 21:25:17 -0400

    hotfix: convert template string

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -2,7 +2,9 @@ function commentReplyFunction() {
   const commentReplies = document.querySelectorAll('.reply');
   console.log(commentReplies);
   function commentReplyExpand(event) {
-    const expandSection = document.querySelector(`.expandme[data-commentid="${this.dataset.commentid}"]`);
+    const expandSelector = ".expandme[data-commentid='" + this.dataset.commentid + "']";
+    console.log(expandSelector);
+    const expandSection = document.querySelector(expandSelector);
     if (expandSection.classList.contains('hidden')) {
       expandSection.classList.remove('hidden');
     } else {
